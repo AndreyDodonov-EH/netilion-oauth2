156d713e9298c058effcfa8152e3bb3a1b39b97d
Andrey Dodonov <aadodonov@gmail.com>
2019-09-28 19:14:37 +0200

    Correct refresh bug

--- CAuth.js
+++ CAuth.js
@@ -89,11 +89,37 @@ module.exports = class CAppAuth {
         });
     }
 
+    refresh() {
+        this.authServer.post(
+            '/token',
+            queryString.stringify(Object.assign({}, {
+                client_id: this.clientId,
+                client_secret: this.clientSecret,
+                refresh_token: this.refreshToken
+            }, {grant_type: 'refresh_token'})),
+            {
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded'
+                }
+            }
+        )
+            .then((response) => {
+                if (this.autoRefresh === true) {
+                    this.saveToken(response.data);
+                }
+            })
+            .catch((err) => {
+                console.log(err);
+                // try to refresh in one second
+                setTimeout(this.refresh.bind(this), 1000);
+            })
+    }
+
     saveToken(data) {
         console.log('NEW TOKEN SAVED');
         this.token = data.access_token;
         this.refreshToken = data.refresh_token;
-        setTimeout(this.Refresh.bind(this), data.expires_in*1000*this.advanceRefresh);
+        setTimeout(this.refresh.bind(this), data.expires_in*1000*this.advanceRefresh);
         if (this.tokenRefreshCallback !== undefined && typeof this.tokenRefreshCallback === 'function') {
             this.tokenRefreshCallback(this.token);
         }
